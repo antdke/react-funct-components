10562459503d42af11d713a7be22dd05587e207c
Anthony Diké <anthonydike@10-16-90-70.dynapool.wireless.nyu.edu>
2019-06-11 11:13:08 -0400

    Adding example 1

---
+++ example1.js
@@ -0,0 +1,12 @@
+// Functional components are basically JavaScript functions being used as React functions which return JSX
+
+import React from "react";
+
+function App() {
+  const greeting = "Hello Functional Component";
+
+  return <h1>{greeting}</h1>;
+}
+
+// As opposed to a React Class Component (rcc), React Functional Components (rfc) have no render functions
+// That's because everything defined within the body of the an rfc IS the render function and returns JSX in the end
