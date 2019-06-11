fbf4ff186194363239e8cbdfd46943b5316c9c38
Anthony Diké <anthony.dke@gmail.com>
2019-06-11 12:25:17 -0400

    Test commit

--- components/example2.js
+++ components/example2.js
@@ -5,9 +5,14 @@ import React from "react";
 
 function App() {
   const greeting = "Hello Universe";
+
+  // rendering Headline component within App component
+  // and passing in the greeting variable as a prop into the
+  // Headline component to use
   return <Headline value={greeting} />;
 }
 
+// Headline component takes the props passed in from App and displays it
 function Headline(props) {
   return <h1>{props.value}</h1>;
 }
@@ -15,3 +20,4 @@ function Headline(props) {
 // Props are rfc parameters
 // The component stays generic and we decide from the outside what it should render (or how it should behave)
 // "rendering a component" = having a component within the body of another component
+// Test
